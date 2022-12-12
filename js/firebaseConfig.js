380e00dc9f5a359a984bf9b633630e68c690ae99
Druplesnubb <druplesnubb@gmail.com>
2022-12-12 23:49:26 +0100

    firebase stuff

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,5 +1,5 @@
-/*// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/*const firebaseConfig = {
   apiKey: "AIzaSyAs7GalW54XkuYyB2spFbERICqN5fWJ68U",
   authDomain: "currencyconversioncheckup.firebaseapp.com",
   databaseURL: "https://currencyconversioncheckup-default-rtdb.firebaseio.com",
@@ -8,5 +8,16 @@ const firebaseConfig = {
   messagingSenderId: "169016477448",
   appId: "1:169016477448:web:c772cbbfd215b7bfdc8ebd",
   measurementId: "G-SSKFYLBJBC"
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZDHhrJKqaAHxolSl1oiodUPFUjDUgn0o",
+  authDomain: "currency-checker-6f3ff.firebaseapp.com",
+  databaseURL: "https://currency-checker-6f3ff-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "currency-checker-6f3ff",
+  storageBucket: "currency-checker-6f3ff.appspot.com",
+  messagingSenderId: "417087864011",
+  appId: "1:417087864011:web:1edcb47a64d789b7d8af4e"
 };
-  firebase.initializeApp(firebaseConfig);*/
+
+firebase.initializeApp(firebaseConfig);
